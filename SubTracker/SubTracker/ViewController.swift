c48636d8bbbb550ad6a373532d15c31332cb013a
Molly Benjaminson <benjaminsonmolly@gmail.com>
2019-02-16 20:56:19 -0800

    Storyboard layout and app icons. Pretty pretty app.

--- SubTracker/SubTracker/ViewController.swift
+++ SubTracker/SubTracker/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addSubButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func remindButton(_ sender: UIButton) {
+    }
 }
 
